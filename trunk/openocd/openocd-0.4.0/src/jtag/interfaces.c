14dcb649a9a69317e0cf8298340373258bc836e2
cahya.wirawan <cahya.wirawan@500fb71a-fe1d-11de-9748-a16e6a44286a>
2010-04-10 00:17:29 +0000

    first support of openocd-0.4.0 for eStick

--- trunk/openocd/openocd-0.4.0/src/jtag/interfaces.c
+++ trunk/openocd/openocd-0.4.0/src/jtag/interfaces.c
@@ -91,6 +91,9 @@ extern struct jtag_interface rlink_interface;
 #if BUILD_ARMJTAGEW == 1
 extern struct jtag_interface armjtagew_interface;
 #endif
+#if BUILD_ESTICK == 1
+extern struct jtag_interface estick_interface;
+#endif
 #endif // standard drivers
 
 /**
@@ -150,6 +153,9 @@ struct jtag_interface *jtag_interfaces[] = {
 #endif
 #if BUILD_ARMJTAGEW == 1
 		&armjtagew_interface,
+#endif
+#if BUILD_ESTICK == 1
+    &estick_interface,
 #endif
 #endif // standard drivers
 		NULL,
